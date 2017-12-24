38404983988de3732d69fb34f8e05292a84893d1
sfill70 <sergfill70@yandex.ru>
2017-12-24 13:52:10 +0300

    3603

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task36.task3603;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/* 
+Замени следующие слова на нужные:
+1. ClassNameToBeReplaced - имя класса, потокобезопасный аналог ArrayList, в котором все операции изменения (mutative operations) используют новую копию основного массива.
+
+2. methodNameToBeReplaced - имя метода, который в текущий список 'list' добавляет те элементы переданной коллекции, которые не содержатся в 'list'.
+
+Не оставляй закомментированный код.
+*/
+public class Solution {
+    public static void main(String... args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.remove("B");
+        List<String> collection = Arrays.asList(new String[]{"B", "C", "D", "B"});
+
+        list.addAllAbsent(collection);
+
+        for (String string : list) {
+            System.out.println(string);
+        }
+
+    }
+}
